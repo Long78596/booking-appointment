6542c2e754ee7e693ff16bd7a1e4c6e9b497de05
Thanh Long <longkolp16@gmail.com>
2024-10-19 19:26:26 +0700

    cấu hình backend, tạo model doctor, user

---
+++ backend/routes/doctorRoute.js
@@ -0,0 +1,9 @@
+// import express from "express";
+// import { addDoctor } from "../controllers/adminController";
+// import upload from "../middlewares/multer";
+
+
+// const adminRoutes=express.Router()
+// adminRoutes.post("/add-doctor", upload.single("image"),addDoctor)
+
+// export default adminRoutes;
